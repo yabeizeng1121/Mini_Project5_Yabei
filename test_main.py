3104cd0e6acfb9fec267fce681f5327ee7eaa697
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-30 15:33:38 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,8 +3,17 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+from mylib.extract import extract
+from mylib.transform_load import load
+from mylib.query import query
+import os
 
+def test_extract():
+    file_path = extract()
+    assert os.path.exists(file_path)
 
-def test_add():
-    assert add(1, 2) == 3
+def test_load():
+    load()
+
+def test_query():
+    result = query()
