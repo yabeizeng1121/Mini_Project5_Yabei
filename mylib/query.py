d219a8ea8c4adc85bb4d090e5d046febf2567014
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-30 15:05:29 -0400

    Update query.py

--- mylib/query.py
+++ mylib/query.py
@@ -5,7 +5,7 @@ import sqlite3
 
 def query():
     """Query the database for the top 5 rows of the CarsDB table"""
-    conn = sqlite3.connect("GroceryDB.db")
+    conn = sqlite3.connect("CarsDB.db")
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM CarsDB LIMIT 5")
     print("Top 5 rows of the CarsDB table:")
