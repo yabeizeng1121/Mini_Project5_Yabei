32ed0b65a7e640c3a0223089b0663ca99cb6ee4a
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-30 14:44:12 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -9,17 +9,15 @@ import os
 
 #load the csv file and insert into a new sqlite3 database
 def load(dataset="/workspaces/sqlite-lab/data/cars.csv"):
-    """"Transforms and Loads data into the local SQLite3 database"""
+    """Transforms and Loads data into the local SQLite3 database"""
 
-    #prints the full working directory and path
-    print(os.getcwd())
     payload = csv.reader(open(dataset, newline=''), delimiter=',')
     conn = sqlite3.connect('GroceryDB.db')
     c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS GroceryDB")
-    c.execute("CREATE TABLE GroceryDB (id,general_name, count_products, ingred_FPro, avg_FPro_products, avg_distance_root, ingred_normalization_term, semantic_tree_name, semantic_tree_node)")
-    #insert
-    c.executemany("INSERT INTO GroceryDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
+    c.execute("DROP TABLE IF EXISTS CarsDB")
+    # Update the columns below based on the structure of cars.csv
+    c.execute("CREATE TABLE CarsDB (Brand TEXT, Price REAL, Body TEXT, Mileage INTEGER, EngineV REAL, Engine Type TEXT, Registration TEXT, Year INTEGER, Model TEXT)")
+    c.executemany("INSERT INTO CarsDB (Brand, Price, Body, Mileage, EngineV, Engine_Type, Registration, Year, Model) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", payload)
     conn.commit()
     conn.close()
     return "GroceryDB.db"
