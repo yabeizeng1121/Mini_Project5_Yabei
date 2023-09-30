b413762dbbcb20b808eb10baf963eec885135d68
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-30 14:37:56 -0400

    Update extract.py

--- mylib/extract.py
+++ mylib/extract.py
@@ -5,7 +5,7 @@ food dataset
 """
 import requests
 
-def extract(url="https://github.com/yabeizeng1121/Mini_Project5/blob/main/cars.csv", 
+def extract(url="https://raw.githubusercontent.com/yabeizeng1121/Mini_Project5/main/cars.csv", 
             file_path="cars.csv"):
     """"Extract a url to a file path"""
     with requests.get(url) as r:
