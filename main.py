3a4e357a87f1d496662b79b5a89def1749e52d71
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-30 15:29:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 """
 ETL-Query script
 """
-
+import sys
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
 
-# Extract
-print("Extracting data...")
-extract()
+def main():
+    try:
+        # Extract
+        print("Extracting data...")
+        source_url = input("Enter the source URL (or press Enter for default): ") or None
+        extract(url=source_url)
+        print("Data extraction completed successfully.\n")
 
-# Transform and load
-print("Transforming data...")
-load()
+        # Transform and Load
+        print("Transforming and loading data...")
+        dataset_path = input("Enter the dataset path (or press Enter for default): ") or None
+        load(dataset=dataset_path)
+        print("Data transformation and loading completed successfully.\n")
 
-# Query
-print("Querying data...")
-query()
+        # Query
+        print("Querying data...")
+        query()
+        print("Data querying completed successfully.\n")
+
+    except Exception as e:
+        print(f"An error occurred: {e}", file=sys.stderr)
+
+if name == "main":
+    main()
